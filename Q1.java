55874106594bf6c9d3f81e71d2c04238d7c1a09a
powcoder <powcoder@163.com>
2022-11-21 21:00:31 +0800

    Comp251 Assignment3

---
+++ Q1.java
@@ -0,0 +1,19 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import java.util.*;
+
+
+public class Q1 {
+
+	public static int find_exit(String[][][] jail) {
+
+	}
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Q2.java
@@ -0,0 +1,18 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import java.util.*;
+
+
+public class Q2 {
+	
+	public static int rings(Hashtable<Integer, ArrayList<Integer>> graph, int[]location) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
